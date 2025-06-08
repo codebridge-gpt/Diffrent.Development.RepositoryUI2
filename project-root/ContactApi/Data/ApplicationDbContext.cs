15c99e3e40539776620c7e8d8349eb6cad08912b
Abhitosh Kumar <kumarabhitosh678@gmail.com>
2025-06-08 16:58:42 +0530

    Auto commit message:

---
+++ project-root/ContactApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
