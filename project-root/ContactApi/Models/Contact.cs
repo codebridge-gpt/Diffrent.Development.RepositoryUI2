b3c338cfb07851dec6e468344405f9771ea40f37
Abhitosh Kumar <kumarabhitosh678@gmail.com>
2025-06-09 17:19:05 +0530

    Auto commit message:

--- project-root/ContactApi/Models/Contact.cs
+++ project-root/ContactApi/Models/Contact.cs
@@ -5,11 +5,14 @@ namespace ContactApi.Models
     public class Contact
     {
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
+
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
         [Required]
         public string Message { get; set; }
     }
